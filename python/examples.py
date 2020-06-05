145ca2db68ac861384bc4de43635158d6ce49ef7
M Feroz Nawrozy <nmfirooz@gmail.com>
2020-06-06 01:03:12 +0300

    2nd user_input sum function done

--- python/examples.py
+++ python/examples.py
@@ -1,4 +1,26 @@
-from math import *
+#2nd example user_range sum 
+"""def rangee(user_range):
+    
+    pre_num = 0
+
+    for cur_num in range(user_range):
+        summ = cur_num + pre_num
+        print("current number " + str(cur_num) + " pervious number " + str(pre_num) + " sum: " + str(summ))
+        pre_num = cur_num
+        cur_num += 1
+
+user_range = int(input("enter a range to do the function : "))
+rangee(user_range)"""
+
+
+
+
+
+
+
+
+#1 example average function
+"""from math import *
 
 
 def average(num1, num2):
@@ -10,5 +32,5 @@ nd_num = float(input("enter second number: "))
 
 avg_result = average(st_num,  nd_num)
 
-print(" average is equal to " , avg_result)
+print(" average is equal to " , avg_result)"""
 
