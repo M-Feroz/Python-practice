432cffcb5db99cdcbc0582325e2acde233c9e8b1
M Feroz Nawrozy <nmfirooz@gmail.com>
2020-06-07 14:10:50 +0300

    Till 9th basic exercise done

--- python/2nd part examples.py
+++ python/2nd part examples.py
@@ -1,8 +1,95 @@
 
 
+#999 exaple palindrome check
+"""
+org_num = int(input("enter the number: "))
+str_num = str(org_num)
+reversed_str = str_num[:: -1]
+print("reversed number is: ", reversed_str)
+if str_num == reversed_str:
+    print("If It is palindrome. ", True)
+else:
+    print("If It is palindrome. ", False)"""
+
+
+    
+    
 
 
 
+'''n=int(input('enter the columns and raws of the matrix: '))
+B= [list(map(int,input('enter matrix').split()))for i in range(n)]
+A = []
+
+for i in range(n):
+    for j in range(n):
+        if j<i and B[i][j]>0 :
+            A.append(B[i][j])
+
+print(B)
+'''
+'''
+user_range = int(input("enter the range: "))
+
+
+for rows in range(user_range):
+    row_column = [list(map(int,input(" enter rows and columns: ").split()))]
+    rows = row_column
+print(rows)
+print("\n")'''
+'''
+#7th Lab:
+#Mohammad Feroz Nawrozy
+
+n=int(input('enter the columns and raws of the matrix: '))
+B= [list(map(int,input('enter matrix').split()))for i in range(n)]
+A=[]
+for i in range(n):
+    for j in range(n):
+        if j<i and B[i][j]>0 :
+            A.append(B[i][j])
+for i in range(n):
+    for j in range(n):
+        print(B[i][j],end=' ')
+    print()
+if len(A)>0:
+    index_max=0
+    index_min=0
+    maxx=A[0]
+    minn=A[0]
+    for i in range(len(A)):
+        if A[i]>maxx:
+            maxx=A[i]
+            index_max=i
+        if A[i]<minn:
+            minn=A[i]
+            index_min=i
+    A[index_max],A[index_min]=A[index_min],A[index_max]
+    print(A)
+else:
+    print('net polozhenie elementov.')'''
+        
+
+
+
+
+#888888 pattern print
+'''user_range = int(input("input number: "))
+for num in range(user_range):
+    for i in range(num):
+        print (num, end=" ") #print number
+    # new line after each row to display pattern correctly
+    print("\n")'''
+
+
+#7777777 example word count in a string.
+'''def strcount(user_input, wordinstr):
+    print(user_input.count(wordinstr))
+
+
+user_input = str(input(" Enter a text to do further: "))
+wordinstr = str(input("write the word which you want to count: "))
+strcount(user_input, wordinstr)'''
 
 #6th exammplae printing the list numbers which are divisible by 5
 '''
