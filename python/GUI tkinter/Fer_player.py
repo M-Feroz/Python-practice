f50dae3ca7c3c40f055b6590a5a4ada4b46f6598
M Feroz Nawrozy <nmfirooz@gmail.com>
2020-06-24 13:21:10 +0300

    GUI tk tt music played

--- python/GUI tkinter/Fer_player.py
+++ python/GUI tkinter/Fer_player.py
@@ -1,10 +1,27 @@
 from tkinter import *
+from pygame import mixer
+
 root= Tk()
+
+mixer.init() #intializing the mixer otherwise it doesnt work
+
 root.geometry('400x400')
 root.title("Fer_player")
 root.iconbitmap(r'icon1.ico')
 
-photo = PhotoImage(file = 'btn.png')
-labelphoto = Label(root, image = photo)
-labelphoto.pack()
+#button_1 = 
+text =Label(root,text = "Feroz Music player")
+text.pack()
+play_photo = PhotoImage(file = 'btn.png')
+#labelphoto = Label(root, image = photo)
+#labelphoto.pack()
+def play_music():
+	mixer.music.load("happier.mp3")
+	mixer.music.play()
+	#print("hi it is play button")
+
+playBtn = Button(root, image = play_photo,command = play_music)
+playBtn.pack()
+
 root.mainloop()
+
